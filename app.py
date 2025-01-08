d95d8d9ffb7eef9064ac557d879d5f2379ce9aed
jessicali <ltianxin@umich.edu>
2025-01-08 18:19:54 -0500

    heroku port

--- app.py
+++ app.py
@@ -132,4 +132,5 @@ def filter_data():
 if __name__ == '__main__':
     with app.app_context():
         init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
