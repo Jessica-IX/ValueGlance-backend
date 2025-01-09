e0bc5bba49262215e0bbfb30d100cfdc724f3161
jessicali <ltianxin@umich.edu>
2025-01-08 20:44:02 -0500

    switch to gunicorn

--- app.py
+++ app.py
@@ -132,5 +132,4 @@ def filter_data():
 if __name__ == '__main__':
     with app.app_context():
         init_db()
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port) 
+    #app.run() for development
